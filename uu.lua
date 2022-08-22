9db6318b3ef24d96dfe45a61a8b3199007acdad1
hh2734 <108557274+hh2734@users.noreply.github.com>
2022-08-23 00:18:08 +0300

    Update and rename uu to uu.lua

--- uu.lua
+++ uu.lua
@@ -3,5 +3,5 @@ local whitelistecheck = loadstring(game:HttpGet("https://raw.githubusercontent.c
 if whitelistecheck[game:service('Players').LocalPlayer.UserId] then
   print("yellow blue vase") -- Ваш скрипт
 else
-  game:service('Players').LocalPlayer:Kick('Bruh')
+  game:service('Players').LocalPlayer:Kick('test')
 end
