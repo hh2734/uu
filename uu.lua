dfab14a84f3ab47c13a3857a1c09cece45776384
hh2734 <108557274+hh2734@users.noreply.github.com>
2022-08-23 00:25:15 +0300

    Update uu.lua

--- uu.lua
+++ uu.lua
@@ -1,4 +1,4 @@
-local whitelistecheck = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/White-List/main/DataBase", true))()
+local whitelistecheck = loadstring(game:HttpGet("https://raw.githubusercontent.com/hh2734/uu/main/uu2.lua", true))()
 
 if whitelistecheck[game:service('Players').LocalPlayer.UserId] then
   print("yellow blue vase") -- Ваш скрипт
