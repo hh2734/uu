79f749061b9e903b7561ecda3571c8f0e3a019b0
hh2734 <108557274+hh2734@users.noreply.github.com>
2022-08-23 00:17:07 +0300

    Create uu2.lua

---
+++ uu2.lua
@@ -0,0 +1,9 @@
+userids = {
+-- [айди игрока в роблоксе] = true,
+[1315379030] = true,
+[123231321] = true
+}
+return userids
+
+-- айди игрока это цифры после /users/
+-- https://www.roblox.com/users/123456789/profile
