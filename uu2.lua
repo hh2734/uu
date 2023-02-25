22aca2c07b3956515e10c2e7c7521162716bb3fa
hh2734 <108557274+hh2734@users.noreply.github.com>
2023-02-25 17:18:57 +0300

    Update uu2.lua

--- uu2.lua
+++ uu2.lua
@@ -8,9 +8,10 @@ userids = {
 [4260680921] = true,
 [4260680921] = true,
 [3074931356] = true,
+[2623015604] = true,
 [3740396695] = true
 }
 return userids
 
--- айди игрока это цифры после /users/л
+-- айди игрока это цифры после /users/
 -- https://www.roblox.com/users/123456789/profile
